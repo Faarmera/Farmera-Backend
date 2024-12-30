253f1aacac7de8058d729fce569798efa507cb87
Chumexxx <obasyemeka@gmail.com>
2024-12-30 10:44:38 +0100

    added role and type to signin response

--- Farmerab/models/User.js
+++ Farmerab/models/User.js
@@ -24,7 +24,7 @@ const userSchema = new mongoose.Schema({
     // default: ""
   },
   phonenumber: {
-    type: Number,
+    type: String,
     required: true,
     unique: true,
     minLength: 11
