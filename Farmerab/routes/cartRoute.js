6cfb98b9a8703e312bf75ec7dde5190f08bf8400
Chumexxx <obasyemeka@gmail.com>
2025-01-06 17:45:43 +0100

    correction of protect routes

--- Farmerab/routes/cartRoute.js
+++ Farmerab/routes/cartRoute.js
@@ -4,11 +4,11 @@ const { protectRoute} = require("../middlewares/protectRoute.js");
 const authorize = require('../middlewares/roleCheckMiddleware.js')
 const router = express.Router();
 
-router.get("/user", protectRoute, authorize(['admin', `buyer`, `farmer`]), getUserCart);
-router.get("/get/allCarts", protectRoute, authorize(['admin']), getAllCarts)
-router.post("/add", protectRoute, authorize(['admin', `buyer`, `farmer`]), addToCart);
-router.delete("/delete" , protectRoute, authorize(['admin', `buyer`, `farmer`]), deleteProductFromCart )
-router.patch("/decrease" , protectRoute, authorize(['admin', `buyer`, `farmer`]), decreaseProductFromCart )
-router.delete("/clear" , protectRoute, authorize(['admin', `buyer`, `farmer`]), clearCart )
+router.get("/user", authorize(['admin', `buyer`, `farmer`]), getUserCart);
+router.get("/get/allCarts", authorize(['admin']), getAllCarts)
+router.post("/add", authorize(['admin', `buyer`, `farmer`]), addToCart);
+router.delete("/delete" , authorize(['admin', `buyer`, `farmer`]), deleteProductFromCart )
+router.patch("/decrease" , authorize(['admin', `buyer`, `farmer`]), decreaseProductFromCart )
+router.delete("/clear" , authorize(['admin', `buyer`, `farmer`]), clearCart )
 
 module.exports = router;
--- Farmerab/routes/categoryRoute.js
+++ Farmerab/routes/categoryRoute.js
@@ -4,10 +4,10 @@ const { protectRoute } = require("../middlewares/protectRoute.js");
 const authorize = require('../middlewares/roleCheckMiddleware.js');
 const router = express.Router();
 
-router.post("/create", protectRoute, authorize(['admin']), createCategory);
-router.get("/get/allCategories", protectRoute, authorize(['admin', `buyer`, `farmer`]), getAllCategories);
-router.get("/get/:name",  protectRoute, authorize(['admin', `buyer`, `farmer`]), getCategoryByName);
-router.put("/update/:id", protectRoute, authorize(['admin']), updateCategory);
-router.delete("/delete/:id", protectRoute, authorize(['admin']), deleteCategory);
+router.post("/create", authorize(['admin']), createCategory);
+router.get("/get/allCategories", authorize(['admin', `buyer`, `farmer`]), getAllCategories);
+router.get("/get/:name", authorize(['admin', `buyer`, `farmer`]), getCategoryByName);
+router.put("/update/:id", authorize(['admin']), updateCategory);
+router.delete("/delete/:id", authorize(['admin']), deleteCategory);
 
 module.exports = router;
