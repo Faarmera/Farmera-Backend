1b992ecada203cb2a2e6f0f706e3559427ae3734
Chumexxx <obasyemeka@gmail.com>
2025-01-11 13:46:41 +0100

    routes update

--- Farmerab/src/server.js
+++ Farmerab/src/server.js
@@ -14,18 +14,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, "views")));
 app.use(cookieParser());
-app.use(cors());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  next();
-});
-
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   })
-// );
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 
 app.use(bodyParser.json({ limit: "30mb" }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
