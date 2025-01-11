ff073843a05daef5b19fee67d826f9777b66b66d
Chumexxx <obasyemeka@gmail.com>
2025-01-11 13:51:45 +0100

    cors

--- Farmerab/src/server.js
+++ Farmerab/src/server.js
@@ -14,8 +14,20 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static(path.join(__dirname, "views")));
 app.use(cookieParser());
+// app.use(cors({
+//   origin: 'http://localhost:5173',
+//   credentials: true
+// }));
+
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: function(origin, callback) {
+    const allowedOrigins = ['http://localhost:5173', 'https://farmera-eyu3.onrender.com'];
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
   credentials: true
 }));
 
