b82f5131c05b32d378d1313020cd7f2307970643
Chumexxx <obasyemeka@gmail.com>
2025-01-16 22:58:44 +0100

    add to cart

--- Farmerab/models/Cart.js
+++ Farmerab/models/Cart.js
@@ -20,13 +20,15 @@ const CartItemSchema = mongoose.Schema({
 const cartSchema = mongoose.Schema(
   {
     cartId: { 
-      type: String 
+      type: String,
+      sparse: true, 
+      unique: true
     },
     user: {
       type: mongoose.Schema.Types.ObjectId,
       ref: 'User',
-      unique: true,
-      default: null, 
+      required: false,
+      index: false, 
     },
     cartItems: [CartItemSchema],
     totalBill: {
@@ -39,6 +41,20 @@ const cartSchema = mongoose.Schema(
     timestamps: true,
   }
 );
+
+cartSchema.index({ 
+  cartId: 1, 
+  user: 1 
+}, { 
+  unique: true,
+  sparse: true,
+  partialFilterExpression: {
+    $or: [
+      { cartId: { $exists: true } },
+      { user: { $exists: true } }
+    ]
+  }
+});
 
 const Cart = mongoose.model("Cart", cartSchema);
 
