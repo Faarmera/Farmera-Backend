201a8aa335a568ff4b647ff363c7ce2748013057
Chumexxx <obasyemeka@gmail.com>
2025-02-26 16:57:37 +0100

    netlify deploy

--- Farmerab/src/server.js
+++ Farmerab/src/server.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 
 app.use(cors({
   origin: function(origin, callback) {
-    const allowedOrigins = ['http://localhost:5173', 'http://localhost:5175', 'http://localhost:5174', `https://farm-olive.vercel.app`, 'https://Faarmeeraa.netlify.app' ,  'https://farmera-eyu3.onrender.com'];
+    const allowedOrigins = ['http://localhost:5173', 'http://localhost:5175', 'http://localhost:5174', `https://farm-olive.vercel.app`, 'https://faarmeeraa.netlify.app' ,  'https://farmera-eyu3.onrender.com'];
     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
       callback(null, true);
     } else {
